b0ffbc05c5b9f911635ce6b30918903331d9ae70
jarangit <jaran.dch@gmail.com>
2023-12-31 18:06:04 +0700

    feat: style home page

--- components/organisms/top-assists.tsx
+++ components/organisms/top-assists.tsx
@@ -13,45 +13,52 @@ type Props = {
 const TopAssists = ({ data }: Props) => {
   return (
     <>
-    <Text value="TOP ASSISTS" size="xl" className='font-bold text-center mb-3' />
-    <Column gap='2' className='justify-between'>
-      {data?.response.map((item: any, key: any) => (
-        <div key={key} className='bg-black_bg rounded-lg p-1 px-3 w-full'>
-          <Link href={{
-            pathname: `/player-detail/${item.player.id}`,
-            query: {
-              namePlayer: item.player.name,
-              league: item.statistics[0].league.id,
-            }
-          }} >
-            <Row className='justify-between items-center' gap='2'>
-              <Row gap='2'>
-                <div className='w-fit rounded-md overflow-hidden'>
+      <Text value="TOP ASSISTS" size="xl" className='font-bold text-center mb-3' />
+      <Column gap='2' className='justify-between'>
+        {data?.response.slice(0,10).map((item: any, key: any) => (
+          <div key={key} className='bg-black_bg rounded-lg py-1 px-3 w-full'>
+            <Link href={{
+              pathname: `/player-detail/${item.player.id}`,
+              query: {
+                namePlayer: item.player.name,
+                league: item.statistics[0].league.id,
+              }
+            }} >
+              <Row className='justify-between items-center' gap='2'>
+                <Row gap='2'>
+                  <Text value={key + 1} size="3xl" className="text-2xl bg-black w-8 flex justify-center items-center rounded-md" />
                   <Image
                     alt=''
-                    src={item.player.photo}
-                    width={40}
-                    height={40}
+                    src={item.statistics[0].team.logo}
+                    width={20}
+                    height={20}
                   />
-                </div>
-                <Text value={item.player.name} size='lg' className='font-medium' />
-                <Image
-                  alt=''
-                  src={item.statistics[0].team.logo}
-                  width={20}
-                  height={20}
-                />
+                  <div className='w-fit rounded-md overflow-hidden'>
+                    <Image
+                      alt=''
+                      src={item.player.photo}
+                      width={30}
+                      height={30}
+                    />
+                  </div>
+                  <Row gap='2' className='!items-end'>
+                    <Column className='' gap='0'>
+                      <Text value={item.player.name} size='lg' className='font-medium' />
+                      <Text value={item.statistics[0].team.name} size='xs' className='font-medium text-gray' />
+                    </Column>
+
+                  </Row>
+                </Row>
+                <Row gap='2' className='text-green'>
+                  <Text value={item.statistics[0].goals.assists} size='md' />
+                  <PiFootprintsFill />
+                </Row>
               </Row>
-              <Row gap='2' className='text-green'>
-                <Text value={item.statistics[0].goals.assists} size='md' />
-                <PiFootprintsFill />
-              </Row>
-            </Row>
-          </Link>
-        </div>
-      ))}
-    </Column>
-  </>
+            </Link>
+          </div>
+        ))}
+      </Column>
+    </>
   )
 }
 
--- components/organisms/top-score.tsx
+++ components/organisms/top-score.tsx
@@ -16,8 +16,8 @@ const TopScore = ({ data }: Props) => {
     <>
       <Text value="TOP SCORE" size="xl" className='font-bold text-center mb-3' />
       <Column gap='2' className='justify-between'>
-        {data?.response.map((item: any, key: any) => (
-          <div key={key} className='bg-black_bg rounded-lg p-1 px-3 w-full'>
+        {data?.response.slice(0,10).map((item: any, key: any) => (
+          <div key={key} className='bg-black_bg rounded-lg py-1 px-3 w-full'>
             <Link href={{
               pathname: `/player-detail/${item.player.id}`,
               query: {
@@ -27,21 +27,25 @@ const TopScore = ({ data }: Props) => {
             }} >
               <Row className='justify-between items-center' gap='2'>
                 <Row gap='2'>
-                  <div className='w-fit rounded-md overflow-hidden'>
-                    <Image
-                      alt=''
-                      src={item.player.photo}
-                      width={40}
-                      height={40}
-                    />
-                  </div>
-                  <Text value={item.player.name} size='lg' className='font-medium' />
+                  <Text value={key + 1} size="3xl" className="text-2xl bg-black w-8 flex justify-center items-center rounded-md" />
                   <Image
                     alt=''
                     src={item.statistics[0].team.logo}
                     width={20}
                     height={20}
                   />
+                  <div className='w-fit rounded-md overflow-hidden'>
+                    <Image
+                      alt=''
+                      src={item.player.photo}
+                      width={30}
+                      height={30}
+                    />
+                  </div>
+                  <Column className='' gap='0'>
+                    <Text value={item.player.name} size='lg' className='font-medium' />
+                    <Text value={item.statistics[0].team.name} size='xs' className='font-medium text-gray' />
+                  </Column>
                 </Row>
                 <Row gap='2' className='text-green'>
                   <Text value={item.statistics[0].goals.total} size='md' />
